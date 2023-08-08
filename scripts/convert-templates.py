1fb47f4399c7eff3acac1ca9f2137926ee2e12cf
Ian Fan <ianfan0@gmail.com>
2023-08-08 13:27:09 +0100

    `PromptTemplate`: change to use Jinja templates

--- scripts/convert-templates.py
+++ scripts/convert-templates.py
@@ -8,53 +8,38 @@ can be easily done.
 import json
 from pathlib import Path
 
+import jinja2
 from promptsource.templates import TemplateCollection
 from tqdm.auto import tqdm
 
 
-def unjinja(template: str) -> str:
-    """Change a Jinja template to a format string."""
-    return (
-        template
-        .replace('{{ ', '{')
-        .replace('{{', '{')
-        .replace(' }}', '}')
-        .replace('}}', '}')
-    )
-
-
 ROOT = Path('converted-templates')
+
+env = jinja2.Environment()
 
 converted = 0
 skipped = 0
-to_check = []
 all_templates = TemplateCollection().datasets_templates.items()
 for (dataset, sub_dataset), templates in tqdm(all_templates):
-    # if the Jinja templates could not be fully converted
-    needs_checking = False
-
     new_templates = {}
     for template in templates.templates.values():
         # `template.jinja` has the format `<prompt>|||<answer>`
-        tpl = unjinja(template.jinja.partition('|||')[0].strip())
-        if '{%' in tpl:
-            needs_checking = True
-
-        new_templates[template.name] = d = {'template': tpl}
+        new_templates[template.name] = d = {'template': template.jinja}
 
         choices = template.get_fixed_answer_choices_list()
-        if choices:
-            d['choices'] = choices
-        elif template.answer_choices:
-            if '{%' in template.answer_choices:
-                needs_checking = True
-            d['choices'] = [*map(
-                str.strip,
-                unjinja(template.answer_choices).split('|||'))
-            ]
-        else:  # template is not multiple-choice  (TODO support this)
-            skipped += 1
-            break
+        if not choices:
+            if template.answer_choices:
+                choices = [answer.strip()
+                           for answer in template.answer_choices.split('|||')]
+                try:
+                    for choice in choices:
+                        env.parse(choice)
+                except jinja2.TemplateSyntaxError:
+                    skipped += 1
+                    break
+            else:  # template is not multiple-choice
+                choices = None
+        d['choices'] = choices
     else:  # i.e. not skipped
         # save to disk
         if sub_dataset:
@@ -68,10 +53,5 @@ for (dataset, sub_dataset), templates in tqdm(all_templates):
             json.dump(new_templates, f, indent=1)
 
         converted += 1
-        if needs_checking:
-            to_check.append(file)
 
 print(f'Converted {converted}, skipped {skipped}')
-print(f'{len(to_check)} files should be checked:')
-for file in to_check:
-    print(f'- {file}')
--- src/ccstools/prompts.py
+++ src/ccstools/prompts.py
@@ -12,20 +12,37 @@ from __future__ import annotations
 
 __all__ = ['PromptTemplate', 'load_templates']
 
+from collections.abc import Iterator
 from dataclasses import dataclass
 import json
+from pathlib import Path
 from typing import Any
 
-# TODO work out what the best variable names should be
+from jinja2 import Environment
 
-TEMPLATES_ROOT = 'templates'  # TODO make this more robust
+try:
+    from importlib.resources import as_file, files
+except ImportError:
+    from importlib_resources import as_file, files
+
+_StrPathT = 'str | os.PathLike[str]'
+
+ENV = Environment()
+TEMPLATES_ROOTS = [files('ccstools.templates').joinpath('converted'),
+                   files('ccstools.templates').joinpath('additional')]
+
+
+def _iter_default_roots() -> Iterator[Path]:
+    for root in TEMPLATES_ROOTS:
+        with as_file(root) as path:
+            yield path
 
 
 @dataclass
 class PromptTemplateOutput:
     prompt: str
     answer: str
-    choices: list[str]
+    choices: list[str] | None
 
 
 @dataclass
@@ -37,15 +54,20 @@ class PromptTemplate:
     name : str
         The name of the template.
     template : str
-        The format string for generating the prompt.
+        The Jinja string for generating the prompt.
     choices : list[str]
-        A list of format strings for generating the choices for the
+        A list of Jinja strings for generating the choices for the
         answers.
+    source : str, optional
+        A string denoting the source of the template, usually a file
+        path.
 
     """
     name: str
     template: str
-    choices: list[str]
+    choices: list[str] | None
+
+    source: str | None = None
 
     def __call__(self,
                  example: dict[str, Any],
@@ -68,23 +90,45 @@ class PromptTemplate:
                 The input prompt for the example.
             - answer : str
                 The answer for the example.
-            - choices : list[str]
-                A list of all the choices for the example. The answer
-                is located at the index given by `example['label']`.
+            - choices : list[str] | None
+                A list of all the choices for the example, if available.
+                The answer is located at the index given by
+                `example['label']`.
 
         """
-        choices = tuple(choice.format(**example) for choice in self.choices)
+        if self.choices:
+            choices = tuple(choice.format(**example)
+                            for choice in self.choices)
+        else:
+            choices = None
+
+        jinja_template = ENV.from_string(self.template)
+        if choices:
+            full_prompt = jinja_template.render(**example,
+                                                answer_choices=choices)
+        else:
+            full_prompt = jinja_template.render(**example)
+        prompt, *rest = map(str.strip, full_prompt.split('|||'))
+
+        try:
+            answer = rest[0]
+        except IndexError:
+            answer = choices[example['label']]
+
         return PromptTemplateOutput(
-            prompt=prefix+self.template.format(**example,
-                                               answer_choices=choices),
-            answer=choices[example['label']],
+            prompt=prefix+prompt,
+            answer=answer,
             choices=choices
         )
 
 
+class TemplateLoadingError(Exception):
+    pass
+
+
 def load_templates(dataset: str,
                    sub_dataset: str = '',
-                   root: str = TEMPLATES_ROOT) -> dict[str, PromptTemplate]:
+                   custom_root: _StrPathT = '') -> dict[str, PromptTemplate]:
     """Load templates from a JSON file.
 
     Parameters
@@ -93,7 +137,7 @@ def load_templates(dataset: str,
         The name of the dataset, e.g. 'super_glue'.
     sub_dataset : str, optional
         The name of the sub dataset, if needed, e.g. 'boolq'.
-    root : str, optional
+    custom_root : str, optional
         The path to the root directory containing the templates.
         The template file is located at `<root>/<dataset>.json` or
         `<root>/<dataset>/<sub_dataset>.json` depending on whether
@@ -102,12 +146,35 @@ def load_templates(dataset: str,
     Returns
     -------
     dict[str, PromptTemplate]
-        A dictionary mapping names of templates to the templates
-        themselves.
+        A mapping of names of templates to the templates themselves.
 
     """
-    full_dataset = dataset + (f'/{sub_dataset}' if sub_dataset else '')
-    with open(f'{root}/{full_dataset}.json') as f:
-        templates = json.load(f)
-    return {name: PromptTemplate(name=name, **template)
-            for name, template in templates.items()}
+    templates = {}
+    dataset_filenames = []
+    roots = [Path(custom_root)] if custom_root else _iter_default_roots()
+    for root in roots:
+        if sub_dataset:
+            dataset_file = root / dataset / f'{sub_dataset}.json'
+        else:
+            dataset_file = root / f'{dataset}.json'
+        dataset_filename = str(dataset_file.resolve())
+        dataset_filenames.append(dataset_filename)
+
+        try:
+            with open(dataset_file) as f:
+                tpls = json.load(f)
+        except FileNotFoundError:
+            pass
+        else:
+            for name, template in tpls.items():
+                templates[name] = PromptTemplate(name=name,
+                                                 source=dataset_filename,
+                                                 **template)
+
+    if not templates:
+        raise TemplateLoadingError(
+            'No templates loaded, tried looking in:\n'
+            + '\n'.join(f'- {filename}' for filename in dataset_filenames)
+            + '\nbut they either do not exist or are empty.')
+
+    return templates
