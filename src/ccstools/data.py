cc76987c876b749de0d4d56e5e53c7f57763e178
Ian Fan <ianfan0@gmail.com>
2023-08-15 11:42:03 +0100

    `balance_filter_map_dataset`: allow passing arbitrary arguments.
    Also displays a tqdm progress bar, disable with `show_progress=False`.

--- src/ccstools/data.py
+++ src/ccstools/data.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 __all__ = ['balance_filter_map_dataset']
 
 from collections.abc import Callable
+from tqdm.auto import tqdm
 from typing import Any, TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -13,6 +14,8 @@ def balance_filter_map_dataset(
     examples_per_label: int,
     dataset: datasets.Dataset,
     function: Callable[[dict[str, Any]], dict[str, Any] | None],
+    *,
+    show_progress: bool = True,
     **kwargs: Any
 ) -> datasets.Dataset:
     """Balance a dataset to a specific size while also mapping over it.
@@ -30,29 +33,53 @@ def balance_filter_map_dataset(
         The dataset to process.
     function : Callable[[dict[str, Any]], dict[str, Any] | None]
         The mapping function to call for each example.
+    show_progress : bool, default: True
+        Whether to display a tqdm progress bar showing how many examples
+        have been generated.
     **kwargs : Any
         Any other arguments to pass to `Dataset.map`.
-        This should not contain: `batched` or `batch_size`.
+        This should not contain: `batched`, `batch_size` or `num_proc`.
+
     """
-    for illegal_key in ('batched', 'batch_size'):
+    for illegal_key in ('batched', 'batch_size', 'num_proc'):
         if illegal_key in kwargs:
             raise ValueError(f'Illegal keyword argument: `{illegal_key}`')
 
     # TODO can this be parallalised?
     count_per_label = [0] * dataset.features['label'].num_classes
 
-    def f(batch: dict[str, list[Any]]) -> dict[str, list[Any]]:
-        # unbatch example
-        example = {key: value[0] for key, value in batch.items()}
+    with tqdm(total=examples_per_label * len(count_per_label),
+              desc='Generating dataset',
+              disable=not show_progress) as progress_bar:
+        # hack so that `Dataset.map` can generate a fingerprint
+        class TqdmWrapper:
+            def __init__(self, bar: tqdm): self.bar = bar
+            def __getstate__(self): return {}
+        wrapped_progress_bar = TqdmWrapper(progress_bar)
 
-        if count_per_label[example['label']] < examples_per_label:
-            mapped_example = function(example)
-            if mapped_example is not None:
-                count_per_label[example['label']] += 1
+        def apply_function(batch: dict[str, list[Any]],
+                           *args: Any,
+                           **kwargs: Any) -> dict[str, list[Any]]:
+            # unbatch example
+            example = {key: value[0] for key, value in batch.items()}
 
-                # rebatch mapped example
-                return {k: [v] for k, v in mapped_example.items()}
+            if count_per_label[example['label']] < examples_per_label:
+                mapped_example = function(example, *args, **kwargs)
+                if mapped_example is not None:
+                    count_per_label[example['label']] += 1
+                    wrapped_progress_bar.bar.update(1)
 
-        return {key: [] for key in example}  # empty batch
+                    # rebatch mapped example
+                    return {k: [v] for k, v in mapped_example.items()}
 
-    return dataset.map(f, batched=True, batch_size=1, **kwargs)
+            return {key: [] for key in example}  # empty batch
+
+        mapped_dataset = dataset.map(apply_function,
+                                     batched=True,
+                                     batch_size=1,
+                                     **kwargs)
+
+        # useful for cached datasets
+        progress_bar.update(len(mapped_dataset) - progress_bar.n)
+
+    return mapped_dataset
